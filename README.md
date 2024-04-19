583552893c1da39477d44bf12b5d7a026c695dc9
RobertWesner <robert@wesner.io>
2024-04-19 08:25:58 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+<div align="center">
+    <h1>YouTube Shadow Comment</h1>
+    <p>
+        Checks if your comments are visible to the public
+    </p>
+</div>
+
+---
+
+Due to automatic filters on YouTube, your comment may show up to you but be entirely invisible to the public.
+
+
+## Installation
+
+1. Install a userscript manager. For example, [Tampermonkey](https://www.tampermonkey.net/):
+   - [Firefox](https://addons.mozilla.org/en/firefox/addon/tampermonkey/)<br>
+   - [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)<br>
+   - [Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)<br>
+   - [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)<br>
+
+2. Install this userscript by clicking [this link](../../raw/main/script.user.js).
+
+
+## Usage
+
+Your comments will be automatically checked for visibility.
+If visible, they will be marked with a green background.
+Invisible comments are red.
+
+![direkteinspeisung.png](readme/direkteinspeisung.png)
+
+
+## License
+
+This project is licensed under the [MIT License](../../raw/main/LICENSE.txt).
---
+++ script.user.js
@@ -0,0 +1,129 @@
+// ==UserScript==
+// @name            YouTube Shadow Comment
+// @description     Checks if your comments are visible to the public
+// @version         2024-04-15
+// @author          Robert Wesner (https://robert.wesner.io)
+// @license         MIT
+// @namespace       http://robert.wesner.io/
+// @match           https://www.youtube.com/*
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant           none
+// ==/UserScript==
+
+(() => {
+    document.head.insertAdjacentHTML('beforeend', `<style>
+        body:not([data-ysc-loaded]) tp-yt-iron-dropdown {
+            display: none !important;
+        }
+
+        ytd-comment-view-model #body {
+            padding: 1em;
+            border-radius: 1em;
+        }
+        
+        [data-ysc-invisible-comment="checking"] {
+            background-color: rgba(100, 100, 100, 20%) !important;
+        }
+
+        [data-ysc-invisible-comment="checking"] #published-time-text::after {
+            content: '\\00a0(Checking...)';
+        }
+
+        [data-ysc-invisible-comment="banned"] {
+            background-color: rgba(255, 0, 0, 20%) !important;
+        }
+
+        [data-ysc-invisible-comment="valid"] {
+            background-color: rgba(3, 255, 36, 20%) !important;
+        }
+    </style>`)
+
+    let searchQuery;
+    const waitForCurrentUserInterval = setInterval(() => {
+        document.querySelector('#avatar-btn').click();
+        document.querySelector('#avatar-btn').click();
+
+        if (!document.querySelector('#channel-handle')) {
+            return;
+        }
+
+        searchQuery = `#author-text[href="/${document.querySelector('#channel-handle').innerText}"]`;
+        clearInterval(waitForCurrentUserInterval);
+    }, 1000);
+
+    const waitForCommentInterval = setInterval(() => {
+        if (!document.querySelector('.ytd-comment-simplebox-renderer img#img') || searchQuery === '') {
+            return;
+        }
+
+        clearInterval(waitForCommentInterval);
+
+        // close dropdown if it was left open
+        if (document.querySelector('tp-yt-iron-dropdown[aria-hidden="false"]')) {
+            document.querySelector('#avatar-btn').click();
+        }
+        document.body.setAttribute('data-ysc-loaded', 'loaded');
+
+        let waitForCleanup = false;
+        document.querySelectorAll('#sort-menu tp-yt-paper-item').forEach(
+            element => element.addEventListener('click', () => {
+                waitForCleanup = true;
+                document.querySelectorAll('[data-ysc-invisible-comment]')
+                    .forEach(element => element.removeAttribute('data-ysc-invisible-comment'));
+                setTimeout(() => waitForCleanup = false, 2000);
+            })
+        );
+
+        setInterval(() => {
+            if (!window.location.pathname.endsWith('/watch')) {
+                return;
+            }
+
+            if (waitForCleanup) {
+                return;
+            }
+
+            document.querySelectorAll(searchQuery).forEach(element => {
+                const parent = element.parentNode.parentNode.parentNode.parentNode.parentNode;
+                if (parent.hasAttribute('data-ysc-invisible-comment')) {
+                    return;
+                }
+
+                const commentLink = parent.querySelector('#published-time-text a').href;
+                parent.setAttribute('data-ysc-invisible-comment', 'checking');
+
+                fetch(commentLink)
+                    .then(response => response.text())
+                    .then(text => {
+                        const pos = text.search('"continuationCommand"');
+                        const continuationCommand = text.substring(pos + 32, text.indexOf('"', pos + 32));
+
+                        fetch('https://www.youtube.com/youtubei/v1/next?prettyPrint=false', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                context: {
+                                    client: {
+                                        clientName: 'WEB',
+                                        clientVersion: '2.20240411.09.00'
+                                    }
+                                },
+                                continuation: continuationCommand,
+                            })
+                        })
+                            .then(response => response.json())
+                            .then(json => {
+                                const { payload } = json.frameworkUpdates.entityBatchUpdate.mutations.filter(element => element.payload.hasOwnProperty('commentEntityPayload'))[0];
+
+                                // checks if first fetched comment matches highlighted comment, i.e. comment is visible publicly
+                                parent.setAttribute(
+                                    'data-ysc-invisible-comment',
+                                    commentLink.endsWith(payload.commentEntityPayload.properties.commentId)
+                                        ? 'valid'
+                                        : 'banned'
+                                );
+                            });
+                    });
+            });
+        }, 2000);
+    }, 1000);
+})();
